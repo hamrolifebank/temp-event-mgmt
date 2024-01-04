d88e7389913cce3b01b85b783e84d45735633a72
Pratiksharai-rumsan <pratiksha.rai@rumsan.net>
2024-01-04 16:58:14 +0545

    feat:create post api for the event

--- event/src/events/events.service.ts
+++ event/src/events/events.service.ts
@@ -1,11 +1,78 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class EventsService {
+  constructor(private prisma: PrismaService) {}
   create(createEventDto: CreateEventDto) {
-    return 'This action adds a new event';
+    const {
+      name,
+      contactPhone,
+      contactEmail,
+      location,
+      latitude,
+      longitude,
+      target,
+      isClosed,
+      bloodBank,
+      organizationId,
+      date,
+    } = createEventDto;
+    const [year, month, day] = date.split(',').map(Number);
+    const adjustedMonth = month - 1;
+
+    const [hours, minutes, seconds] = createEventDto.startTime
+      .split(':')
+      .map(Number);
+
+    const [endHours, endMinutes, endSeconds] = createEventDto.endTime
+      .split(':')
+      .map(Number);
+
+    const startTime = new Date(
+      year,
+      adjustedMonth,
+      day,
+      hours,
+      minutes,
+      seconds,
+      0,
+    );
+
+    const endTime = new Date(
+      year,
+      adjustedMonth,
+      day,
+      endHours,
+      endMinutes,
+      endSeconds,
+      0,
+    );
+
+    const dateOfEvent = new Date(date);
+
+    return this.prisma.event.create({
+      data: {
+        name,
+        contactPhone,
+        contactEmail,
+        location,
+        latitude,
+        longitude,
+        target,
+        isClosed,
+        organizationId,
+        date: dateOfEvent,
+        bloodBank,
+        startTime,
+        endTime,
+      },
+    });
+
+    //const dateObject = new Date(createEventDto.date, createEventDto.startTime);
+    //return this.
   }
 
   findAll() {
