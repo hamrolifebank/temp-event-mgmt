40ba6c3c99ca85edf36a583e6877f42f43ace629
Pratiksharai-rumsan <pratiksha.rai@rumsan.net>
2024-01-05 14:17:32 +0545

    feat:add conditional update

--- event/src/events/events.service.ts
+++ event/src/events/events.service.ts
@@ -58,15 +58,58 @@ export class EventsService {
     });
   }
 
-  update(uuid: string, updateEventDto: UpdateEventDto) {
-    const { date, startTime, endTime, contactEmail } = updateEventDto;
+  async update(uuid: string, updateEventDto: UpdateEventDto) {
+    const { date, startTime, endTime } = updateEventDto;
 
-    // return this.prisma.event.update({
-    //   where: {
-    //     uuid,
-    //   },
-    //   data: updateEventDto,
-    // });
+    if (date && startTime && endTime) {
+      const newDate = new Date(date);
+      const start = new Date(`${date}T${startTime}`);
+
+      const end = new Date(`${date}T${endTime}`);
+
+      return this.prisma.event.update({
+        where: { uuid },
+        data: {
+          date: newDate,
+          startTime: start,
+          endTime: end,
+        },
+      });
+    } else if (startTime || endTime) {
+      const findEvent = await this.prisma.event.findUnique({
+        where: { uuid },
+      });
+      const year = String(findEvent.date.getFullYear());
+
+      const month = String(findEvent.date.getMonth() + 1).padStart(2, '0');
+
+      const day = String(findEvent.date.getDate()).padStart(2, '0');
+
+      const newDate = `${year}-${month}-${day}`;
+
+      let start: Date;
+      let end: Date;
+      if (startTime) {
+        start = new Date(`${newDate}T${startTime}`);
+      }
+      if (endTime) {
+        end = new Date(`${newDate}T${endTime}`);
+      }
+      return this.prisma.event.update({
+        where: { uuid },
+        data: {
+          startTime: start,
+          endTime: end,
+        },
+      });
+    } else {
+      return this.prisma.event.update({
+        where: {
+          uuid,
+        },
+        data: updateEventDto,
+      });
+    }
   }
 
   remove(id: number) {
